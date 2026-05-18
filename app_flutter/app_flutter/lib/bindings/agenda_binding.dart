96fe1a926d71c796d1280e66e5ed3c4722da9165
williansteinx <willian.sanini@hotmail.com>
2026-05-18 10:38:11 -0300

    Corrigindo estrutura do repositório

--- app_flutter/app_flutter/lib/bindings/agenda_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import '../controllers/agenda_controller.dart';
-
-class AgendaBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<AgendaController>(() => AgendaController());
-  }
-}
--- app_flutter/app_flutter/lib/controllers/login_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-class LoginController extends GetxController {
-  var isPsicologo = true.obs;
-
-  void mudarPerfil(bool valor) {
-    isPsicologo.value = valor;
-  }
-}
