c0179061714636226f74dbb0a647e50175954358
williansteinx <willian.sanini@hotmail.com>
2026-06-07 21:40:10 -0300

    Remove api_config.dart do rastreamento

--- app_flutter/lib/core/api_config.dart
+++
@@ -1,9 +0,0 @@
-class ApiConfig {
-  static const String _baseUrl =
-      'http://200.19.1.19/20232GR.ADS0003/IntegraPsi/public/index.php';
-
-  static const String usuarios = '$_baseUrl/usuarios';
-  static const String login = '$_baseUrl/usuarios/login';
-  static const String pacientes = '$_baseUrl/pacientes';
-  static const String agendamentos = '$_baseUrl/agendamentos';
-}
